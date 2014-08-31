a180a0c2f41cc328c53878dc2217256735942e99
Kauhsa <kauhsa@kapsi.fi>
2014-08-31 12:45:22 +0300

    Try to publish to GitHub Pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir out; 
+( cd out
+ git init
+ git config user.name "Kauhsa"
+ git config user.email "kauhsa@kapsi.fi"
+ cp ../dist/kandi.pdf ./kandi.pdf
+ cp ../dist/kandi.html ./index.html
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
