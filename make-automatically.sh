2a35fa63dfe867a0b562c2025254cef9d4748a1b
Kauhsa <kauhsa@kapsi.fi>
2014-10-04 14:19:36 +0300

    Automatic make script builds on launch

--- make-automatically.sh
+++ make-automatically.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-fswatch -o src templates | xargs -n1 -I{} make dist/kandi.html dist/kandi.pdf
+func="make dist/kandi.html dist/kandi.pdf"
+$func
+fswatch -o src templates | xargs -n1 -I{} $func
